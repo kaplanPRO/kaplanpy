d275e72d6547e903ce5b47e40a82c0dc1490b9df
Çağatay Onur Şengör <contact@csengor.com>
2021-02-22 13:59:58 +0300

    New function: kaplan.project.Project.analyze

--- kaplan/project.py
+++ kaplan/project.py
@@ -2,9 +2,14 @@
 from lxml import etree
 
 # Standard Python libraries
+import difflib
 import json
 import os
 import zipfile
+
+# Internal Python files
+from .kdb import KDB
+import kaplan
 
 class Project:
     '''
@@ -27,6 +32,89 @@ class Project:
         self.files = project_metadata['files']
         self.translation_memories = project_metadata.get('translation_memories', {})
         self.termbases = project_metadata.get('termbases', {})
+
+    def analyze(self):
+
+        project_entries = []
+
+        project_tm_entries = []
+        for tm_i in self.translation_memories:
+            project_tm_entries += list(KDB(self.translation_memories[tm_i]).get_all_source_entries())
+
+        project_report = {}
+        project_total = {'Repetitions': 0,
+                         '100%': 0,
+                         '95%-99%': 0,
+                         '85%-94%': 0,
+                         '75%-84%': 0,
+                         '50%-74%': 0,
+                         'New': 0,
+                         'Total': 0
+                        }
+
+        for file_i in self.files:
+            file_report = {'Repetitions': 0,
+                           '100%': 0,
+                           '95%-99%': 0,
+                           '85%-94%': 0,
+                           '75%-84%': 0,
+                           '50%-74%': 0,
+                           'New': 0,
+                           'Total': 0
+                          }
+
+            for tu in kaplan.open_bilingualfile(self.files[file_i]['targetBF']).get_translation_units():
+                for segment in tu:
+                    if segment.tag.split('}')[-1] == 'ignorable':
+                        continue
+
+                    source_entry, _ = KDB.segment_to_entry(segment[0])
+                    word_count = len(source_entry.split())
+
+                    sm = difflib.SequenceMatcher()
+                    sm.set_seq2(source_entry)
+
+                    if source_entry in project_entries:
+                        file_report['Repetitions'] += word_count
+                    elif source_entry in project_tm_entries:
+                        file_report['100%'] += word_count
+                        project_entries.append(source_entry)
+                    else:
+                        highest_match = 0.0
+                        for entry in project_entries + project_tm_entries:
+                            sm.set_seq1(entry)
+                            highest_match = max(sm.quick_ratio(), highest_match)
+
+                        if highest_match >= 0.95:
+                            file_report['95%-99%'] += word_count
+                        elif highest_match >= 0.85:
+                            file_report['85%-94%'] += word_count
+                        elif highest_match >= 0.75:
+                            file_report['75%-84%'] += word_count
+                        elif highest_match >= 0.5:
+                            file_report['50%-74%'] += word_count
+                        else:
+                            file_report['New'] += word_count
+
+                        project_entries.append(source_entry)
+
+                    file_report['Total'] += word_count
+
+            project_total['Repetitions'] += file_report['Repetitions']
+            project_total['100%'] += file_report['100%']
+            project_total['95%-99%'] += file_report['95%-99%']
+            project_total['85%-94%'] += file_report['85%-94%']
+            project_total['75%-84%'] += file_report['75%-84%']
+            project_total['50%-74%'] += file_report['50%-74%']
+            project_total['New'] += file_report['New']
+            project_total['Total'] += file_report['Total']
+
+            project_report[self.files[file_i]['name']] = file_report
+
+        if len(self.files) > 1:
+            project_report['Total'] = project_total
+
+        return project_report
 
     def export(self, target_path, files_to_export=None, include_source_and_resources=True):
         if not target_path.lower().endswith('.kpp'):
